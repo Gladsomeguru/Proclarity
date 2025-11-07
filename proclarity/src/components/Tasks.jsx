d4bd7f0ccd53697e224ecba61a94c6a58f83492c
Gurunathan <gurunathan.p@indiumsoft.com>
2025-11-08 01:25:50 +0530

    update add task button position

--- proclarity/src/components/Tasks.jsx
+++ proclarity/src/components/Tasks.jsx
@@ -35,29 +35,26 @@ const Tasks = ({ tasks, setTasks, editingTask, setEditingTask, setOpenModal, ope
         setEditingTask(taskToEdit);
     }
 
+    const addTaskButton = (
+        <button
+            onClick={() => setOpenModal(true)}
+            className="bg-emerald-500 text-slate-100 p-4 py-1 rounded-lg shadow hover:bg-emerald-600 
+      transition-colors duration-200 cursor-pointer 
+      dark:bg-emerald-700 flex flex-row items-center 
+      justify-center gap-2 h-fit lg:w-auto w-full"
+        >
+            <AiOutlinePlus />
+            <span>Add Task</span>
+        </button>
+    );
+
+
     return (
         <>
-            <div className="flex items-center px-4 lg:gap-2 gap-4 flex-wrap">
-                {!openModal && tasks.length !== 0 && !editingTask &&
-                    < button onClick={() => setOpenModal(true)} className="bg-emerald-500 text-slate-100 p-4 py-1 rounded-lg shadow hover:bg-emerald-600 
-                        transition-colors duration-200 cursor-pointer 
-                        dark:bg-emerald-700 flex flex-row items-center 
-                        justify-center gap-2 h-fit"><AiOutlinePlus />
-                        <span>Add Task</span></button>}
-                {/* 
-                <div className="flex bg-red-500 items-center px-2 py-1 rounded-lg text-gray-800 dark:bg-red-500 dark:text-gray-900 gap-2 cursor-pointer">
-                    <span>Pending</span>
-                    <p className="bg-gray-50 text-sm px-1 rounded-md">12</p>
-                </div>
-                <div className="flex bg-yellow-300 items-center px-2 py-1 rounded-lg text-gray-800 dark:bg-yellow-300 dark:text-gray-900 gap-2">
-                    <span>In Progress</span>
-                    <p className="bg-gray-50 text-sm px-1 rounded-md">5</p>
-                </div>
-                <div className="flex bg-yellow-300 items-center px-2 py-1 rounded-lg text-gray-800 dark:bg-yellow-300 dark:text-gray-900 gap-2">
-                    <span>Completed</span>
-                    <p className="bg-gray-50 text-sm px-1 rounded-md">8</p>
-                </div> */}
-
+            <div className="flex items-center px-4 lg:gap-2 gap-4 flex-wrap lg:mb-0 mb-4">
+                {!openModal && tasks.length !== 0 && !editingTask && (
+                    <div className="hidden lg:visible">{addTaskButton}</div>
+                )}
                 <div className="flex gap-2 lg:ms-auto flex-wrap w-full lg:w-auto">
                     <form className="ms-auto text-gray-800 flex grow items-center gap-1 dark:text-emerald-100 cursor-pointer p-2 py-1 border border-emerald-300 outline-emerald-300 rounded-lg focus-within:outline-2 dark:border-slate-600 dark:outline-slate-600 box-border">
                         <input type="text" placeholder="Search tasks..." className="w-full focus:outline-none" /><CiSearch />
@@ -69,9 +66,10 @@ const Tasks = ({ tasks, setTasks, editingTask, setEditingTask, setOpenModal, ope
                 </div>
 
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 lg:max-h-[calc(100%-50px)] max-h-[calc(100%-100px)] p-4 overflow-y-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:max-h-[calc(100%-34px)] max-h-[calc(100%-98px)] 
+            p-4 lg:py-4 py-0 overflow-y-auto">
                 {tasks.map((task) => (
-                    <div key={task.id} className={`rounded-lg w-full max-w-md bg-white dark:bg-slate-900 shadow-md border-0 border-r-6 w-full md:1/5 hover:scale-[1.04] hover:shadow-lg transition-all ease-in-out duration-300
+                    <div key={task.id} className={`rounded-lg w-full max-w-md bg-white dark:bg-slate-900 shadow-md border-0 border-r-6 w-full lg:1/5 hover:scale-[1.04] hover:shadow-lg transition-all ease-in-out duration-300
                 ${task.status === "Completed" ? "border-green-500 dark:border-green-800" : task.status === "In Progress" ? "border-yellow-300 dark:border-yellow-300" : "border-red-500 dark:border-red-800"}`}>
                         <div className="flex flex-col gap-2 rounded-lg p-4 h-full">
                             <div className=" flex flex-row gap-2 items-center">
@@ -113,9 +111,9 @@ const Tasks = ({ tasks, setTasks, editingTask, setEditingTask, setOpenModal, ope
                                         : 'No due date'}</p>
                                 </div>
                                 <div className={`flex flex-row flex-wrap gap-1 items-center p-2 py-1 rounded-lg text-black cursor-default
-                                    ${task.status === "Completed" ? "bg-green-500 dark:bg-green-700 text-white dark:text-gray-100" : task.status === "In Progress" ? 
-                                    "bg-yellow-300 dark:bg-yellow-300" : 
-                                    "text-white bg-red-500 dark:bg-red-800 dark:text-black dark:text-white"}`} title={`Status: ${task.status}`}>
+                                    ${task.status === "Completed" ? "bg-green-500 dark:bg-green-700 text-white dark:text-gray-100" : task.status === "In Progress" ?
+                                        "bg-yellow-300 dark:bg-yellow-300" :
+                                        "text-white bg-red-500 dark:bg-red-800 dark:text-black dark:text-white"}`} title={`Status: ${task.status}`}>
                                     <p className="text-xs font-medium text-nowrap">{task.status}</p>
                                 </div>
                             </div>
@@ -124,6 +122,8 @@ const Tasks = ({ tasks, setTasks, editingTask, setEditingTask, setOpenModal, ope
                 ))
                 }
             </div>
+            <div className="lg:hidden flex justify-center m-4 mb-0">{!openModal && tasks.length !== 0 && !editingTask && addTaskButton}</div>
+
         </>
     )
 }
